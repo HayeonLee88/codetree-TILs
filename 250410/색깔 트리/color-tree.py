2acf4d278a18214484bdc5e16715dc05762962f1
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2025-04-10 15:20:45 +0900

    Title: 색깔 트리, Time: 48ms, Memory: 19MB, Status: Wrong Answer - Codetree

--- 250410/색깔 트리/color-tree.py
+++ 250410/색깔 트리/color-tree.py
@@ -1,100 +1,82 @@
 import sys
+input = lambda: sys.stdin.readline().rstrip()
 
-input = lambda:sys.stdin.readline().rstrip()
-
+# Number of commands
 q = int(input())
 
-
-class Node():
-    def __init__(self, id, p_id, color, max_d, p_depth):
-        self.id = id
+class Node:
+    def __init__(self, m_id, p_id, color, max_d, depth):
+        self.id = m_id
         self.p_id = p_id
         self.color = color
         self.max_d = max_d
-        self.depth = p_depth + 1
-
-    def set_color(self,color):
-        self.color = color
-
-    def set_subtree(self):
-        self.subtree += 1
+        self.depth = depth
 
 tree = [[] for _ in range(100000)]
-
+root = []
 
 def dfs(x, color):
     tree[x][0].color = color
-    if len(tree[x]) == 1:
-        return True
-    else:
-        for sub in tree[x][1:]:
-            sub.color = color
-            dfs(sub.id, color)
-    return True  
+    # If this node has children, recolor them recursively
+    for child in tree[x][1:]:
+        dfs(child.id, color)
 
 answer = 0
 
-def score(x, cnt):
+def score(x, colors_set):
     global answer
-    cnt.add(tree[x][0].color)
+    # Include the color of the current node
+    colors_set.add(tree[x][0].color)
+    # If leaf node
     if len(tree[x]) == 1:
         answer += 1
-        return True
-    else:
-        for sub in tree[x][1:]:
-            tmp = set()
-            cnt.add(sub.color)
-            score(sub.id, tmp)
-            cnt.update(list(tmp))
-        answer += len(cnt) * len(cnt)
-    return answer
+        return
+    # Otherwise, handle children
+    for child in tree[x][1:]:
+        child_colors = set()
+        score(child.id, child_colors)
+        # Merge child’s colors into our set
+        colors_set |= child_colors
+    # Add square of total number of colors
+    answer += len(colors_set) * len(colors_set)
 
-
-root = []
 for _ in range(q):
     cmds = list(map(int, input().split()))
     cmd = cmds[0]
+
     if cmd == 100:
+        # 100 m_id p_id color max_d
         _, m_id, p_id, color, max_d = cmds
         if p_id == -1:
-            root.append(Node(m_id, p_id, color, max_d, 0))
-            tree[m_id].append(root[-1])
+            # Create a root node
+            new_node = Node(m_id, p_id, color, max_d, 1)
+            root.append(new_node)
+            tree[m_id].append(new_node)
         else:
-            if len(tree[p_id]) == 1:
-                now = tree[p_id][0]
-                check = True
-                while True:
-                    if now.depth + 1 > now.max_d:
-                        check = False
-                        break
-                    if now.p_id == -1:
-                        break
-                    now = tree[now.p_id][0]
-                    
-                if check:
-                    new = Node(m_id, p_id, color, max_d, tree[p_id][0].depth)
-                    tree[m_id].append(new)
-                    tree[p_id].append(new)
-                    now = tree[p_id][0]
-                    while True:
-                        if now.p_id == -1:
-                            break
-                        now.depth += 1
-                        now = tree[now.p_id][0]
+            # Attach child to existing node
+            parent_node = tree[p_id][0]
+            # Check if adding child exceeds parent's max_d
+            if parent_node.depth + 1 <= parent_node.max_d:
+                new_node = Node(m_id, p_id, color, max_d, parent_node.depth + 1)
+                tree[m_id].append(new_node)
+                tree[p_id].append(new_node)
             else:
-                new = Node(m_id, p_id, color, max_d, tree[p_id][0].depth)
-                tree[m_id].append(new)
-                tree[p_id].append(new)
+                # If it violates max depth, do nothing or handle differently
+                pass
 
     elif cmd == 200:
+        # 200 m_id color
         _, m_id, color = cmds
         dfs(m_id, color)
-                  
+
     elif cmd == 300:
+        # 300 m_id
         _, m_id = cmds
         print(tree[m_id][0].color)
-    else:
+
+    elif cmd == 400:
+        # reset and compute answer across all roots
         answer = 0
-        for x in root:
-            score(x.id, set())
+        for rt in root:
+            score(rt.id, set())
         print(answer)
