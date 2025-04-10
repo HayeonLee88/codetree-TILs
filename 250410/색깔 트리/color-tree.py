b9e2d364be69fc3ba0fe85302892c90e7cec9a55
Hayeon Lee <83398511+HayeonLee88@users.noreply.github.com>
2025-04-10 16:20:09 +0900

    Title: 색깔 트리, Time: 545ms, Memory: 31MB, Status: Runtime Error - Codetree

--- 250410/색깔 트리/color-tree.py
+++ 250410/색깔 트리/color-tree.py
@@ -1,3 +1,31 @@
+'''
+12:55~
+(1) 노드추가
+Node: idx, parent_id, color, max_d
+color: 빨 1, 주 2, 노 3, 초 4, 파 5
+if Parent_id == -1: Node = root
+max_depth: subtree의 최대 깊이, 자기자신은 1
+    - 기존 노드의 Max_depth와 충돌이 되는 새로운 노드는 연결하지 않음
+자신의 subtree를 알고 있어야 함.
+
+100 idx p_id color max_d
+
+(2) 색깔 변경
+idx의 모든 subtree의 색을 idx 노드의 색으로 바꿈
+
+200 idx color
+
+(3) 색깔 조회
+특정 노드 idx의 현재 색을 조회
+
+300 idx
+
+(4) 점수 조회
+모든 노드의 가치를 계산하여 가치 제곱의 합을 출력
+각 노드의 가치는 해당 노드를 루트로 하는 서브트리 내 서로 다른 색깔의 수로 정의
+
+400
+'''
 import sys
 
 input = lambda:sys.stdin.readline().rstrip()
@@ -26,8 +54,14 @@ def dfs(x, color):
     tree[x][0].color = color
     for sub in tree[x][1:]:
         dfs(sub.id, color)
-    return True  
 
+
+'''
+5(4): 19(3), 10(2): 1(1), 9(1) : 4(2) : 14(1)
+2(1)
+16(1)
+
+'''
 answer = 0
 
 def score(x, cnt):
@@ -35,7 +69,6 @@ def score(x, cnt):
     cnt.add(tree[x][0].color)
     if len(tree[x]) == 1:
         answer += 1
-        return True
     else:
         for sub in tree[x][1:]:
             tmp = set()
@@ -57,26 +90,32 @@ for _ in range(q):
             tree[m_id].append(root[-1])
         else:
             if len(tree[p_id]) == 1:
+                depth = 1
                 now = tree[p_id][0]
                 check = True
                 while True:
-                    if now.depth + 1 > now.max_d:
+                    if depth + 1 > now.max_d:
                         check = False
                         break
                     if now.p_id == -1:
                         break
                     now = tree[now.p_id][0]
+                    depth += 1
                     
                 if check:
-                    new = Node(m_id, p_id, color, max_d)
-                    tree[m_id].append(new)
-                    tree[p_id].append(new)
                     now = tree[p_id][0]
+                    depth = 1
                     while True:
-                        now.depth += 1
+                        if depth + 1 > now.depth:
+                            now.depth += 1
                         if now.p_id == -1:
                             break
                         now = tree[now.p_id][0]
+                        depth += 1 
+                    new = Node(m_id, p_id, color, max_d)
+                    tree[m_id].append(new)
+                    tree[p_id].append(new)
+
             else:
                 new = Node(m_id, p_id, color, max_d)
                 tree[m_id].append(new)
